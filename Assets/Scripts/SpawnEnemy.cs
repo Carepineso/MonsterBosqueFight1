cb241b582353d08367924df341289311165e73fb
PabloVlls <jpvlls13@gmail.com>
2022-08-27 22:48:15 -0500

    prefabs aleatorios enem y parc

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class SpawnEnemy : MonoBehaviour
 {
     public Transform[] spawnPaints;
-    public GameObject prefabEnemy;
+    public GameObject[] prefabEnemy;
     private int random = 0;
+    private int enemigos = 0;
     public bool can_sp = true;
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,8 @@ public class SpawnEnemy : MonoBehaviour
         {
             yield return new WaitForSeconds(Random.Range(4.0f, 14.0f));
             random = Random.Range(0, spawnPaints.Length);
-            GameObject gameObject = Instantiate<GameObject>(prefabEnemy, spawnPaints[random].position, Quaternion.identity);
+            enemigos = Random.Range(0, prefabEnemy.Length);
+            GameObject gameObject = Instantiate<GameObject>(prefabEnemy[enemigos], spawnPaints[random].position, Quaternion.identity);
         }
     }
 }
--- Assets/Scripts/spawn.cs
+++ Assets/Scripts/spawn.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class spawn : MonoBehaviour
 {
     public Transform[] spawnPaints;
-    public GameObject prefabParcero;
+    public GameObject[] prefabParcero;
     private int random= 0;
+
+    private int parcero= 0;
     public bool can_sp = true;
     // Start is called before the first frame update
     void Start()
     {
-         StartCoroutine(TSpawn());
+        StartCoroutine(TSpawn());
     }
 
     IEnumerator TSpawn()
@@ -21,7 +23,8 @@ public class spawn : MonoBehaviour
         {
             yield return new WaitForSeconds(Random.Range(4.0f, 14.0f));
             random = Random.Range(0, spawnPaints.Length);
-            GameObject gameObject = Instantiate<GameObject>(prefabParcero, spawnPaints[random].position, Quaternion.identity);
+            parcero= Random.Range(0, prefabParcero.Length);
+            GameObject gameObject = Instantiate<GameObject>(prefabParcero[parcero], spawnPaints[random].position, Quaternion.identity);
         }
     }
 }
