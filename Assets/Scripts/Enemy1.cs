75fd9731d88fbdec7b525368853c0269e8c13c07
PabloVlls <jpvlls13@gmail.com>
2022-08-28 21:28:13 -0500

    mecanicas3

--- Assets/Scripts/Enemy1.cs
+++ Assets/Scripts/Enemy1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy1 : MonoBehaviour
 {
     public Estados estado;
     public float patrolSpeed = 0f;
@@ -194,3 +194,4 @@ public enum Estados
     patrol,
     muerto
 }
+
